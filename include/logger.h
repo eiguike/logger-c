5813a536f7a67c41fe939fba52d5f716c97101cd
ricke <rick.eihara@gmail.com>
2019-05-09 00:50:43 -0300

    Feat: Add macros to create logs messages

--- include/logger.h
+++ include/logger.h
@@ -1,6 +1,11 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
+#define INFO(Format, ...) Logger->Info(Format, ##__VA_ARGS__);
+#define DEBUG(Format, ...) Logger->Debug(Format, ##__VA_ARGS__);
+#define WARN(Format, ...) Logger->Warning(Format, ##__VA_ARGS__);
+#define ERROR(Format, ...) Logger->Error(__FUNCTION__, __LINE__, Format, ##__VA_ARGS__);
+
 typedef enum OutputType {
   STDOUT,
   BINARY_FILE
@@ -17,4 +22,5 @@ typedef struct LOGGER_STRUCTURE {
 int Logger_New(OUTPUT Output, ...);
 
 extern LOGGER* Logger;
+
 #endif
