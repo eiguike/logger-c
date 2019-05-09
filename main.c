c3fd6a5ebe2eb60d9e0469d6d6d82a20a91f824f
ricke <rick.eihara@gmail.com>
2019-05-09 00:51:00 -0300

    Refact: Add examples

--- main.c
+++ main.c
@@ -8,15 +8,19 @@ extern LOGGER* Logger;
 
 int main() {
   Logger_New(BINARY_FILE, "test.log");
-  Logger->Info("This is a information message!");
-  Logger->Warning("This is a warning message!");
-  Logger->Error(__FUNCTION__, __LINE__, "This is a error message!");
+  INFO("Hy my name is %s project!", "Logger-c")
+  INFO("I can do math! %d + %d = %d", 2, 2, 4)
+  DEBUG("This is a debug message!");
+  WARN("This is a warning message!");
+  ERROR("Serious! This is a error message!");
   Logger->Dispose();
 
   Logger_New(STDOUT);
-  Logger->Info("This is a information message!");
-  Logger->Warning("This is a warning message!");
-  Logger->Error(__FUNCTION__, __LINE__, "This is a error message!");
+  INFO("Hy my name is %s project!", "Logger-c")
+  INFO("I can do math! %d + %d = %d", 2, 2, 4)
+  DEBUG("This is a debug message!");
+  WARN("This is a warning message!");
+  ERROR("Serious! This is a error message!");
   Logger->Dispose();
 
   return 0;
